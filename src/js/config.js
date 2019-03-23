25a44ee1edafcefc8d8d525c333466ecc13edb70
Molham <dr.molham93@gmail.com>
2019-03-23 09:11:01 +0100

    ADD: Additional API Key

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
 export const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-export const apiKey = '1469b8ad9c6f546dffaaac901805a9bf';
+export const apiKey = '1469b8ad9c6f546dffaaac901805a9bf';   //e833d0d9507cd9e6e090dfe8b9e8aec3
